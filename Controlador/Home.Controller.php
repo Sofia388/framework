3998ae4f931f01d66250a05e3c56bd0a42eac9b0
Sofia388 <62312117+Sofia388@users.noreply.github.com>
2020-06-11 10:43:38 -0600

    Add files via upload

---
+++ Controlador/Home.Controller.php
@@ -0,0 +1,25 @@
+<?php
+  /**
+   * Clase home que permite la primera carga del sistema
+   */
+  class Home
+  {
+     function __construct()
+     {
+      //code...
+	 }
+   public function Inicio()
+    {
+        $smart=new Smarty();
+        $smarty->display('Default.tpl');
+    }
+    
+
+    public function Fun2()
+    {
+        echo "En controlador Home y metodo Fun2";
+    }
+
+  }
+
+ ?>
---
+++ templates/Default.tpl
@@ -0,0 +1,6 @@
+{include file="Cabeceras\header.tpl"}
+  <div align="center">
+  <br>
+    <h1>Hello, world!</h1>
+    </div>
+    {include file="Cabeceras\Footer.tpl"}
